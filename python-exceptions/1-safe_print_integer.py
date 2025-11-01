917ab60bfb84fddfcd65b98207b749f2f95df0ad
Hashimo77 <hagamirz7@gmail.com>
2025-11-01 12:49:54 +0000

    AsdJJ

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """Prints an integer using '{:d}'.format() and returns True if successful."""
+    """Print an integer if possible and return True, else return False."""
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError):
+    except Exception:
         return False
