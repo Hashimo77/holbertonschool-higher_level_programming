c111e815ebf7b3a53b718cd4153a1b0ef3cdcdc4
Hashimo77 <hagamirz7@gmail.com>
2025-11-01 13:16:29 +0000

    asdJ

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,10 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
+            # Integer olmayan elementləri atırıq
             continue
         except IndexError:
+            # Listdən kənara çıxsaq döngünü dayandırırıq
             break
     print()
     return count
