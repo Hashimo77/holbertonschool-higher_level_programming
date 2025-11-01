213ef58c3f88713017ba353ff41675d99084d88a
Hashimo77 <hagamirz7@gmail.com>
2025-11-01 13:17:37 +0000

    Asdks

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,13 +4,13 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            # Integer olmayan elementləri atırıq
-            continue
+            value = my_list[i]
+            try:
+                print("{:d}".format(value), end="")
+                count += 1
+            except (ValueError, TypeError):
+                continue
         except IndexError:
-            # Listdən kənara çıxsaq döngünü dayandırırıq
             break
     print()
     return count
