b1c6de33d69db6da5441072410ef012f600eee27
Hashimo77 <hagamirz7@gmail.com>
2025-11-01 12:22:26 +0000

    Adksja

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """Update the dictionary with the key/value pair or add it if it doesn't exist."""
+    a_dictionary[key] = value
+    return a_dictionary
