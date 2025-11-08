a20932eca1e7b612c8f3c8a9be282f1eab67eb58
Hashimo77 <hagamirz7@gmail.com>
2025-11-08 12:19:17 +0000

    AsdjsadsKJ

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,62 +1,9 @@
-#!/usr/bin/python3
-"""Defines a class Square"""
-
-
-class Square:
-    """Class that defines a square"""
-
-    def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new Square
-        Args:
-            size (int): size of the square
-            position (tuple): position of the square
-        """
-        self.size = size
-        self.position = position
-
-    @property
-    def size(self):
-        """Retrieve the size"""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Set the size with validation"""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    @property
-    def position(self):
-        """Retrieve the position"""
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        """Set the position with validation"""
-        if (
-            not isinstance(value, tuple)
-            or len(value) != 2
-            or not all(isinstance(num, int) for num in value)
-            or not all(num >= 0 for num in value)
-        ):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
-    def area(self):
-        """Return the area of the square"""
-        return self.__size ** 2
-
-    def my_print(self):
-        """Prints the square using '#' with position"""
-        if self.__size == 0:
-            print("")
-            return
-
-        # position[1] → yuxarıdan boş sətir sayı
-        for _ in range(self.__position[1]):
-            print("")
-
-        # hər sətirdə pos
+def my_print(self):
+    """Prints the square with the character '#'"""
+    if self.__size == 0:
+        print("")
+        return
+    for _ in range(self.__position[1]):
+        print("")
+    for _ in range(self.__size):
+        print(" " * self.__position[0] + "#" * self.__size)
