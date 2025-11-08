2ce0b3c8e0ff34fb654b632031ec9fe7e896e1d9
Hashimo77 <hagamirz7@gmail.com>
2025-11-08 16:26:13 +0000

    Asdjasdjk

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines a function that checks if an object is an instance of a
+class that inherited (directly or indirectly) from the specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False.
+    """
+    if type(obj) is not a_class and isinstance(obj, a_class):
+        return True
+    return False
