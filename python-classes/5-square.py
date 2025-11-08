8f837ad8fc9d327345b9375c572fc9b271767537
Hashimo77 <hagamirz7@gmail.com>
2025-11-08 12:16:48 +0000

    Asdksajd

---
+++ python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Class that defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square
+        Args:
+            size (int): The size of the square
+        """
+        self.size = size  # setter vasitəsilə yoxlanır
+
+    @property
+    def size(self):
+        """Retrieve the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square
+        Args:
+            value (int): The size to set
+        Raises:
+            TypeError: If value is not an integer
+            ValueError: If value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with the character '#'"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
