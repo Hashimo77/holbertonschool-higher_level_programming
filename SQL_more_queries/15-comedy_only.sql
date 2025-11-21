f7312fe6d6aa01105f645ce93c8ea1981cd30a61
Hashimo77 <hagamirz7@gmail.com>
2025-11-21 11:51:59 +0000

    asdKJ

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,9 @@
--- 15-comedy_only.sql
--- List all shows that belong to the Comedy genre
+-- Lists all Comedy shows
+-- Usage: cat 15-comedy_only.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
 
-SELECT t.title
-FROM tv_shows t
-JOIN tv_show_genres tg ON t.id = tg.tv_show_id
-JOIN genres g ON tg.genre_id = g.id
-WHERE g.name = 'Comedy'
-ORDER BY t.title ASC;
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
