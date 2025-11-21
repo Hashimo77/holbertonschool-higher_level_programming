50da3fea059bfddb6623e550e9aaea586c7dd108
Hashimo77 <hagamirz7@gmail.com>
2025-11-21 11:49:19 +0000

    AskjdKJ

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,8 @@
--- List all tv shows without a genre
+-- Lists all shows without any linked genre
+-- Usage: cat 12-no_genre.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
