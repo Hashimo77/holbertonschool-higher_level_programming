f533e46157a4873209720662965bfae43c4c4683
Hashimo77 <hagamirz7@gmail.com>
2025-11-21 11:47:28 +0000

    Asdkl

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
--- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Lists all shows with at least one linked genre
+-- Usage: cat 10-genre_id_by_show.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
