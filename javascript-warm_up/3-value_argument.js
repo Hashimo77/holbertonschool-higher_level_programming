0c628dc402ede15934204831b2808ee48b936a47
Hashimo77 <hagamirz7@gmail.com>
2025-11-30 11:47:18 +0000

    sg

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+const process = require('process');
+
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,7 @@
+const process = require('process');
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
